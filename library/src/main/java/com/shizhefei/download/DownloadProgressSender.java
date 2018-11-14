45ff826768d90cf7d2e0fa36194acfac9dc468a9
LuckyJayce <794629068@qq.com>
2018-11-14 09:10:08 +0800

    删除无用的类

--- library/src/main/java/com/shizhefei/download/DownloadProgressSender.java
+++
@@ -1,9 +0,0 @@
-package com.shizhefei.download;
-
-import com.shizhefei.mvc.ProgressSender;
-
-public abstract class DownloadProgressSender implements ProgressSender {
-    public abstract void onHttpConnect(int httpCode, String saveFileName, String tempFileName, String contentType, long current, long contentLength);
-
-    public abstract void onProgress(long currentSize, long contentLength);
-}
